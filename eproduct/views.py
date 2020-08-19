d2d481b9b801b2cb468d81f7d2bdabdff2809e2b
AgentCody333 <67738217+AgentCody333@users.noreply.github.com>
2020-08-19 09:08:26 +0100

    Update views.py

--- eproduct/views.py
+++ eproduct/views.py
@@ -6,3 +6,10 @@ from. models import Item
 def index(request):
     a = Item.objects.all
     return render(request, 'index.html',{'pro':a})
+
+
+def searchbar(request):
+    if request.method == 'GET' :
+        search = request.GET.get('search')
+        post = Item.objects.all().filter(fruit_name = search)
+        return render(request, 'search.html', {'post':post})
